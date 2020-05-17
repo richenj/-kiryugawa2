7dec74038f84c14fc18be09e280def784f5c52c6
richenj <3280093362@qq.com>
2020-05-17 16:11:22 +0800

    fix router

--- dist/sw.js
+++ dist/sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/kiryugawa2/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/kiryugawa2/'), new workbox.strategies.NetworkFirst ({}), 'GET')
